1d0df55bb289749ccc0004b56d143ee2ada58e47
Barış Deniz Sağlam <bdsaglam@gmail.com>
2018-07-18 20:23:06 +0300

    stack file added

---
+++ src/stack.js
@@ -0,0 +1,15 @@
+const stack = {
+  "Region": "eu-west-1",
+  "UserPoolClientId": "11o6cafvh34or5qiitna8jecob",
+  "IdentityPoolId": "eu-west-1:35c557dc-83d0-4a44-a43b-e6f4e396ba58",
+  "UserPoolId": "eu-west-1_CF22T3UPG",
+  "ServiceEndpoint": "https://8i1gkw8ja5.execute-api.eu-west-1.amazonaws.com/dev",
+  "ListLambdaFunctionQualifiedArn": "arn:aws:lambda:eu-west-1:189128138749:function:pupp-api-dev-list:1",
+  "GetLambdaFunctionQualifiedArn": "arn:aws:lambda:eu-west-1:189128138749:function:pupp-api-dev-get:1",
+  "CreateLambdaFunctionQualifiedArn": "arn:aws:lambda:eu-west-1:189128138749:function:pupp-api-dev-create:1",
+  "UpdateLambdaFunctionQualifiedArn": "arn:aws:lambda:eu-west-1:189128138749:function:pupp-api-dev-update:1",
+  "DeleteLambdaFunctionQualifiedArn": "arn:aws:lambda:eu-west-1:189128138749:function:pupp-api-dev-delete:1",
+  "ServerlessDeploymentBucketName": "pupp-api-dev-serverlessdeploymentbucket-1h7pkmdl5sn4q"
+};
+
+export default stack;
